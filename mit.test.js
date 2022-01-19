cdaa4bc2a8a166bb00587ea0a87fb96895579e15
Joshua Cordoba <jcordobab101@icloud.com>
2022-01-19 08:16:56 -0600

    Practice mitfacts exercise corrections

--- mit.test.js
+++ mit.test.js
@@ -1,4 +1,4 @@
-var utils  = require('course-utilities');
+var utils = require('course-utilities');
 var mit = utils.load('./mit.js', 'mit');
 
 describe('MIT', () => {
@@ -14,15 +14,19 @@ describe('MIT', () => {
     });
 
     test('Colors match', () => {
-        expect(mit.colors).toContain('Silver Gray');    
+        expect(mit.colors).toContain('Silver Gray');
     });
 
     test('Founded range', () => {
         expect(mit.founded).toBeLessThanOrEqual(1875);
-        expect(mit.founded).toBeGreaterThanOrEqual(1850);    
+        expect(mit.founded).toBeGreaterThanOrEqual(1850);
     });
 
     test('Motto', () => {
         expect(mit.motto).toMatch('Mens');
     });
+
+    test('Mascot', () => {
+        expect(mit.mascot).toMatch('Beaver');
+    })
 });
